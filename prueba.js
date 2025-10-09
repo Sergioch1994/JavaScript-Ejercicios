3ae826f458d9f739e8fc0cc100c3b4d356e4c806
Sergio Chica <Sergioch1994@gmail.com>
2025-10-09 13:20:39 +0200

    JavaScript Clase

---
+++ prueba.js
@@ -0,0 +1,16 @@
+let name = "Sergio";
+
+
+
+//La persona y su color
+
+let coche = {
+
+    marca: "Nissan",
+    tipo: "Deportivo",
+    precio: "15$",
+    stock: 33,
+
+}
+
+console.log(`el coche de la marca ${coche.marca}, es un ${coche.tipo}, cuyo valor es ${coche.precio} y su stock es ${coche.stock}`)
