a8daf13b8909fc1b86de1c66ce4b73b9497d93a6
Sergio Chica <Sergioch1994@gmail.com>
2025-10-16 11:22:04 +0200

    Ejercicios clase

--- prueba.js
+++ prueba.js
@@ -14,3 +14,40 @@ let coche = {
 }
 
 console.log(`el coche de la marca ${coche.marca}, es un ${coche.tipo}, cuyo valor es ${coche.precio} y su stock es ${coche.stock}`)
+
+//Ejercicio 1
+
+const nombres = ["Isaac", "Moises", "Víctor", "Alejandro"];
+
+console.log(nombres);
+
+//Ejercicio 2
+
+
+
+
+
+//Ejercicio 3
+
+const persona = {
+    nombre: "Pepe",
+    edad: "15",
+    ciudad: "Berlín",
+    saludar: function() {
+        console.log(`Hola qué tal mi nombre es ${this.nombre}!`);
+    }
+};
+
+console.log(persona.saludar());
+
+//Ejercicio 4
+
+const frutas = ["pera", "aguacate", "manzana"];
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(`El índice es ${i}), fruta: ${frutas[i]}`)
+}
+
+mostrarArray(frutas);
+mostrarArray(["pera", "aguacate","manzana"]);
+
