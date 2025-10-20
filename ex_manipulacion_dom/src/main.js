3b806e05cc72bbfc5af7882d74ffe5499cebb2df
Sergio Chica <Sergioch1994@gmail.com>
2025-10-20 14:00:02 +0200

    Commit

--- ex_manipulacion_dom/src/main.js
+++ ex_manipulacion_dom/src/main.js
@@ -1,2 +1,30 @@
 import './style.css'
 
+// EX 1 
+
+const textEx1 = document.getElementById("textEx1")
+console.log(textEx1);
+
+const h1Text = document.querySelectorAll(".h1Ex1")
+console.log(h1Text);
+
+// EX 2
+
+
+const ex2 = document.querySelectorAll("#divEx2");
+const divEx2 = document.createElement("div")
+const NewPEx2
+
+
+
+// EX 3
+
+
+
+
+
+
+
+
+
+
